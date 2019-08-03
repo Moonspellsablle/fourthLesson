49b6d408c6b0a6c966d2fb74cd6d71bcbf75df87
“Vladyslav” <moonspellsablle@gmail.com>
2019-08-03 21:28:53 +0300

    Added initialization for symbolsCharArray. Added const variable. Replaced for loop with supporting pointers.

--- swap.cpp
+++ swap.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <stdint.h>
+#include <cstdint>
 
 void swap(char& first_elem, char& last_elem) {
   char temp = first_elem;
@@ -10,26 +10,21 @@ void swap(char& first_elem, char& last_elem) {
 int main() {
   std::cout << "Enter a string (I will revert it): ";
   const uint16_t MAX_COUNT_OF_SYMBOLS = 150;
-  char symbolsCharArray[MAX_COUNT_OF_SYMBOLS];
+  char symbolsCharArray[MAX_COUNT_OF_SYMBOLS]{};
   std::cin >> symbolsCharArray;
 
-  uint16_t halfOfLenArray;
   uint16_t lenOfLine;
 
-  for (size_t i = 0; i < MAX_COUNT_OF_SYMBOLS; ++i) {
-    if (symbolsCharArray[i] == '\0') {
-      halfOfLenArray = (i / 2);
-      lenOfLine = i;
-      break;
-    }
+  for (size_t i = 0; symbolsCharArray[i] != '\0'; ++i) {
+    lenOfLine = (i + 1);
   }
 
-  for (size_t i = 0; i < halfOfLenArray; ++i) {
-    swap(symbolsCharArray[i], symbolsCharArray[(lenOfLine - 1) - i]);
+   for (char *begin = symbolsCharArray, *end = symbolsCharArray + (lenOfLine  - 1);
+       begin != end; ++begin, --end) {
+    swap(*begin, *end);
   }
 
   std::cout << symbolsCharArray << std::endl;
 
   return 0;
 }
-

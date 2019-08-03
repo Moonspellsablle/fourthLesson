6cbb58b904b5cab148be1214a89f914e3db7b838
“Vladyslav” <moonspellsablle@gmail.com>
2019-08-03 18:00:13 +0300

    Added initialization for symbolsCharArray. Added const variable. Changed 'range for' cycle to 'for' cycle.

--- isDigit.cpp
+++ isDigit.cpp
@@ -2,20 +2,20 @@
 
 int main() {
   std::cout << "Enter a string (I will grep all numbers from it): ";
-  char symbolsCharArray[150];
+  const short LEN_OF_SYMBOLS = 150;
+  char symbolsCharArray[LEN_OF_SYMBOLS]{};
   std::cin >> symbolsCharArray;
 
   const char LOWER_DIGIT_RANGE = '1';
   const char HIGH_DIGIT_RANGE = '9';
-  for (auto singleCharFromLine: symbolsCharArray) {
-    if (singleCharFromLine == '\0') {
-      break;
-    }
-    if ((singleCharFromLine >= LOWER_DIGIT_RANGE) && (singleCharFromLine <= HIGH_DIGIT_RANGE)) {
-      std::cout << singleCharFromLine;
+  for (size_t i = 0;  symbolsCharArray[i] != '\0'; ++i) {
+    if ((symbolsCharArray[i] >= LOWER_DIGIT_RANGE) && (symbolsCharArray[i] <= HIGH_DIGIT_RANGE)) {
+      std::cout << symbolsCharArray[i];
     }
   }
   std::cout << std::endl;
 
   return 0;
 }
+
+

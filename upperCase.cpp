389df33b6087b9c45e69355a5b8964b761f33e28
“Vladyslav” <moonspellsablle@gmail.com>
2019-08-03 20:01:19 +0300

    Added initialization for symbolsCharArray. Added const variable. Changed 'range for' cycle to 'for' cycle. Broke the string into 3 parts

--- upperCase.cpp
+++ upperCase.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <stdint.h>
+#include <cstdint>
 
 int main() {
   std::cout << "Enter a string (I will change register to UPPER format): ";
-  char symbolsCharArray[150];
+  const uint16_t LEN_OF_SYMBOLS = 150;
+  char symbolsCharArray[LEN_OF_SYMBOLS]{};
   std::cin >> symbolsCharArray;
 
   const char ASCII_UPPER_CASE_RANGE_STARTS_FROM = 'a';
   const char ASCII_UPPER_CASE_RANGE_END_TO = 'z';
   const uint16_t SHIFT = 32;
 
-  for (auto singleCharFromLine: symbolsCharArray) {
-    if (singleCharFromLine == '\0') {
-      break;
-    }
-    std::cout << (((singleCharFromLine >= ASCII_UPPER_CASE_RANGE_STARTS_FROM) && (singleCharFromLine <= ASCII_UPPER_CASE_RANGE_END_TO)) ? char(singleCharFromLine - SHIFT) : singleCharFromLine);
+  for (size_t i = 0;  symbolsCharArray[i] != '\0'; ++i) {
+    std::cout << (((symbolsCharArray[i] >= ASCII_UPPER_CASE_RANGE_STARTS_FROM) && \
+    (symbolsCharArray[i] <= ASCII_UPPER_CASE_RANGE_END_TO)) ? \
+    char(symbolsCharArray[i] - SHIFT) : \
+    symbolsCharArray[i]);
   }
   std::cout << std::endl;
   return 0;
